fa3872361e6ae0b6d5c68212b0c12aa5f742f508
kurasakishoki <211020@std.hi-joho.ac.jp>
2024-09-24 16:13:49 +0900

    現在地にマーカーを置く

--- sotuken/urls.py
+++ sotuken/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('map/', views.map, name='map'),
+
+
 ]
--- sotuken/views.py
+++ sotuken/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 
 def index(request):
   return render(request, 'index.html')
+
+
+def map(request):
+  return render(request, 'map.html')
+
