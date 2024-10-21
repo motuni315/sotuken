1c0c4c9de5f82127d88014cd3ce9e11406b48f7d
kurasakishoki <211020@std.hi-joho.ac.jp>
2024-10-21 11:27:24 +0900

    画面追加

--- sotuken/urls.py
+++ sotuken/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
   path('', views.index, name='index'),
   path('map/', views.map, name='map'),
   path('search/', views.search, name='search'),
+  path('register/', views.register, name='register'),
+  path('chat/', views.chat, name='chat'),
+  path('notice/', views.notice, name='notice'),
+  path('route/', views.route, name='route'),
 
 ]
--- sotuken/views.py
+++ sotuken/views.py
@@ -13,7 +13,20 @@ def map(request):
 
 def search(request):
   if request.method == 'GET':
-    search_condition = request.GET.get['search-condition']
+    search_condition = request.GET.get('search-condition')
     if search_condition == "場所":
       search = request.GET.get('search')
     return render(request,'map.html',{'search':search})
+
+def register(request):
+   return render(request,'register.html')
+
+def chat(request):
+  return render(request,'chat.html')
+
+def route(request):
+  return render(request,'route.html')
+
+
+def notice(request):
+  return render(request,'notice.html')
