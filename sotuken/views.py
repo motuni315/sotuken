f50b9687c015b4acc11fc61abc73ec3e2cfc477e
kurasakishoki <211020@std.hi-joho.ac.jp>
2024-10-21 15:48:41 +0900

    マップ検索、検索結果の詳細情報追加と経路情報追加

--- sotuken/views.py
+++ sotuken/views.py
@@ -11,12 +11,14 @@ def index(request):
 def map(request):
   return render(request, 'map.html')
 
+
 def search(request):
   if request.method == 'GET':
     search_condition = request.GET.get('search-condition')
-    if search_condition == "場所":
-      search = request.GET.get('search')
-    return render(request,'map.html',{'search':search})
+    if search_condition:
+      if search_condition == "場所":
+        search = request.GET.get('search')
+    return render(request, 'map.html', {'search': search})
 
 def register(request):
    return render(request,'register.html')
